9c3f7aa9c7ca05313b7aef478b07956aa19d6256
dragon lab <83529946+yongwoo1207@users.noreply.github.com>
2022-04-27 14:30:56 +0900

    Add files via upload

---
+++ 6ch/programming project/8/programming_project.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int days, start_day, i, j;
+	printf("Enter number of days in month: ");
+	scanf_s("%d", &days);
+
+	printf("Enter sterting day of the week (1=sun, 7=sat): ");
+	scanf_s("%d", &start_day);
+
+	//첫 줄 공백 만들기
+	for (i = 1; i < start_day; i++)
+		printf("   ");
+	
+	//달력 만들기
+	for (j = 1; j <= days; j++, i++) {
+		printf("%3d", j);
+		if (i % 7 == 0)
+			printf("\n");
+	}
+
+	return 0;
+}
